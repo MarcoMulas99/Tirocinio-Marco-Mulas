21330c0bdb7edce1ffceb0bbc45b99c1480d4dbe
MarcoMulas99 <mrc.mls99@gmail.com>
2021-05-14 15:51:39 +0200

    Creazione tabelle con gli orari settimanali per studenti, professori e corsi

--- linkedPage.js
+++ linkedPage.js
@@ -1,11 +1,120 @@
-let data = JSON.parse(localStorage.getItem("fileData"));
-console.log(data);
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const type = urlParams.get('type');
+const selectedElement = urlParams.get('selectedElement');
+
+const data = JSON.parse(localStorage.getItem("fileData"));
+
+const nD = data.nD;
+const nH = data.nH;
+let filteredTimeTable;
 
 
-/*let type = localStorage.getItem("type");
-let selectedElement = localStorage.getItem("selectedElement");
+switch (type) {
+  case 'CLASSES':
+    filteredTimeTable = data.TIMETABLE.filter(e => {return e.class === selectedElement});
+    break;
+  case 'TEACHERS':
+    filteredTimeTable = data.TIMETABLE.filter(e => {return e.teacher === selectedElement});
+    break;
+  case 'COURSES':
+    const course = data.COURSES.find(e => e.name === selectedElement);
+    filteredTimeTable = data.TIMETABLE.filter(e => {return e.class === course.class && e.teacher === course.teacher});
+  break;
+}
 
-console.log(type);
-console.log(selectedElement);*/
 
+function createTableMatrix(timeTable, nD, nH) {
+  let matrix = [];
+  for(var i=0; i<nH; i++) {
+    matrix[i] = new Array(nD);
+  }
 
+  timeTable.forEach(item => {
+    matrix[item.period-1][item.day-1]=item;
+  });
+
+  return matrix;
+}
+
+function createTable(matrix, type){
+
+  let table = document.createElement('TABLE');
+	table.border = '1';
+  let tr;
+	let td;
+
+  tr = document.createElement('TR');
+  table.appendChild(tr);
+
+  for(let i = 0; i<=6; i++){
+    td = document.createElement('TD');
+    switch(i){
+			case 0:
+				td.innerHTML = ""
+				break;
+			case 1:
+				td.innerHTML = "Lunedì"
+				break;
+			case 2:
+				td.innerHTML = "Martedì"
+				break;
+			case 3:
+				td.innerHTML = "Mercoledì"
+				break;
+      case 4:
+        td.innerHTML = "Giovedì"
+        break;
+      case 5:
+        td.innerHTML = "Venerdì"
+        break;
+      case 6:
+        td.innerHTML = "Sabato"
+        break;
+		}
+		//console.log(td.innerHTML);
+		tr.appendChild(td);
+  }
+
+  console.log(matrix);
+
+  for (i = 0; i<matrix.length; i++){
+    tr = document.createElement('TR');
+
+    td = document.createElement('TD');
+    td.innerHTML = i+1;
+
+    tr.appendChild(td);
+
+    table.appendChild(tr);
+
+    for(j = 0; j<matrix[i].length; j++){
+      td = document.createElement('TD');
+      tr.appendChild(td);
+      if(typeof matrix[i][j] !== 'undefined'){
+        switch (type) {
+          case 'CLASSES':
+            td.innerHTML = matrix[i][j].teacher;
+            break;
+          case 'TEACHERS':
+            td.innerHTML = matrix[i][j].class;
+            break;
+          case 'COURSES':
+            td.innerHTML = matrix[i][j].teacher+"-"+matrix[i][j].class;
+          break;
+        }
+      }else{
+        td.innerHTML = "";
+      }
+    }
+  }
+  return table;
+}
+
+let body = document.getElementById('body');
+body.appendChild(createTable(createTableMatrix(filteredTimeTable, nD, nH), type));
+
+//document.write(JSON.stringify(createTableMatrix(filteredTimeTable, nD, nH)));
+
+//document.write(JSON.stringify(filteredTimeTable));
--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function dataManagement(data){
 	courses.appendChild(createTable(data.COURSES, "COURSES"));
 
 
-	
+
 }
 
 
@@ -53,11 +53,9 @@ function createTable(list, elementsType){
 	let td;
 	let a;
 
-	let i = 0
 	let maxColumns = 10;
 
-	while(i<list.length){
-		
+	for (var i = 0; i < list.length; i++) {
 		if(i % maxColumns === 0){
 			tr = document.createElement('TR');
 			table.appendChild(tr);
@@ -65,11 +63,11 @@ function createTable(list, elementsType){
 
 		td = document.createElement('TD');
 		a = document.createElement('A');
-		a.href = "./linkedPage.html";
-		
-		
 
-		
+
+
+
+
 
 		if(typeof list[i] != "string"){
 			a.innerHTML = list[i].name;
@@ -78,111 +76,11 @@ function createTable(list, elementsType){
 			a.innerHTML = list[i]
 		}
 
-		/*a.onclick = () => {
-			window.alert(a.innerHTML);
-			localStorage.setItem("type", elementsType);
-			localStorage.setItem("selectedElement", a.innerHTML);
-		}*/
+		a.href = "./linkedPage.html?"+"type="+elementsType+"&selectedElement="+a.innerHTML;
 
 		td.appendChild(a);
 		tr.appendChild(td);
-		i++;
 	}
 
 	return table;
 }
-
-
-
-
-/*let createTable = (orari) => {
-	let myTable = document.getElementById('myTable');
-
-	let table = document.createElement('TABLE');
-	table.border = '1';
-
-	let tr = document.createElement('TR');
-	let td;
-
-	let i;
-
-	for(i = 0; i<=orari.nPeriodi; i++){
-		td = document.createElement('TD');
-		switch(i){
-			case 0:
-				td.innerHTML = ""
-				break;
-			case 1:
-				td.innerHTML = "Lunedì"
-				break;
-			case 2:
-				td.innerHTML = "Martedì"
-				break;
-			case 3:
-				td.innerHTML = "Mercoledì"
-				break;
-		}
-		console.log(td.innerHTML);
-		tr.appendChild(td);
-	}
-
-	table.appendChild(tr);
-
-	for(i = 0; i<orari.nPeriodi; i++){
-		tr = document.createElement('TR');
-		table.appendChild(tr);
-
-		td = document.createElement('TD');
-		td.innerHTML = `${i+1}°`;
-		tr.appendChild(td);
-
-		
-		let aux;
-
-		aux = findElement(i+1, orari.orario);
-
-		console.log(aux);
-		console.log("---------");
-
-		let day;
-		let element
-		for(j = 0; j<orari.nGiorni; j++){
-			day;
-
-			switch(j){
-				case 0:
-					day="LUN"
-					break;
-				case 1:
-					day="MAR"
-					break;
-				case 2:
-					day="MER"
-					break;
-			}
-
-			td = document.createElement('TD');
-
-			element = aux.find(element => element.giorno === day);
-
-			if (element!=undefined){
-				td.innerHTML = element.materia;
-			}else td.innerHTML = "";
-
-			tr.appendChild(td);
-		}		
-		
-	}
-	myTable.appendChild(table);}
-function findElement(ora, array){
-
-	let aux = [];
-
-	for(i=0; i<array.length; i++){
-		if(array[i].ora === ora){
-			aux.push(array[i]);
-		}
-	}
-
-	return aux;}*/
-
